1fc669d3172916a96e666aaede45fd0ca772528a
Gabriele99-developer <79506019+Gabriele99-developer@users.noreply.github.com>
2021-06-10 11:15:21 +0200

    sistemato index_dev.js

--- src/index_dev.js
+++ src/index_dev.js
@@ -1,4 +1,1 @@
-
-alert('Ora sembra tutto un casino ma alla fine sarà più chiaro.. spero!')
-console.info('Va bene Fabio, se lo dici tu..')
-// le funzioni asincrone si dichiarano aggiungendo async prima dell'espressione
+console.log(process.env.API_KEY)
